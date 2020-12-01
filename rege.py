1ad3d0dc8b3601504ad621138105e6e75832003a
Tushar <tusharmehta08@outlook.com>
2020-12-01 20:28:59 +0530

    First commit

---
+++ rege.py
@@ -0,0 +1,9 @@
+#Email extractor
+
+import re
+
+str= "ADGITM, NEW DELHIFC-26, SHASTRI PARK, DELHI hello@gmail.com yoyo@outlook.com – 110 053 contact@adgitmdelhi.ac.in +91(11) 49905900-99, 32526261-64 http:/www.adgitmdelhi.ac.in"
+
+email = re.findall(r"[0-9a-zA-Z._%]+@[0-9a-zA-Z._%]+[.][a-zA-Z.0-9]+", str )
+
+print(email)
